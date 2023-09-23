8b62e5676bc576b795d08a5ba8ede773e85005e7
KyLLlHEP <kushnerartur@gmail.com>
2023-09-23 17:50:00 +0100

    feat: add loop in code (check input value )

--- assess_tasks/js/calcvat_script.js
+++ assess_tasks/js/calcvat_script.js
@@ -2,9 +2,35 @@
 let amount = document.getElementById('amount');
 let calculateVatButton = document.getElementById('calcbtn');
 
+
+function isValidInput(input) {
+  let hasDecimalPoint = false;
+
+  for(let i = 0; i < input.length; i++) {
+    let char = input[i];
+
+    if(char === '.' && !hasDecimalPoint) {
+      hasDecimalPoint = true;
+      continue;
+    }
+    if(!Number.isFinite(+char)) {
+      return false;
+    }
+  }
+  return true;
+}
+
 function calculateVAT() {
   // let amountValue = +amount.value; 
   let amountValue = parseFloat(amount.value);
+
+  if(!isValidInput(amount.value)) {
+    alert("Put a number please.");
+    amount.style.borderColor= 'red';
+    return;
+  } else {
+    amount.style.borderColor = "";
+  }
 
   let vat22 = amountValue * 0.22;
   let vat = document.getElementById('vat');
