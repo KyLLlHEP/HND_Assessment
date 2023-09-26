d177ced1b8a979a0c288b0e7c18b6a975d36cf92
KyLLlHEP <kushnerartur@gmail.com>
2023-09-26 10:35:30 +0100

    feat: Add logical in function

--- js/nursery-script.js
+++ js/nursery-script.js
@@ -1,11 +1,24 @@
 let menuselectElm = document.getElementById("menuselect");
 
 menuselectElm.addEventListener("change",function(){
+  let selectDay = menuselectElm.value.toUpperCase();
 
-})
+  if (selectDay === "MONDAY") {
+    document.getElementById("msgout").innerHTML = "Monday's child is fair of face";
+  } else if (selectDay === "TUESDAY") {
+    document.getElementById("msgout").innerHTML = "Tuesday's child is full of grace";
+  } else if (selectDay === "WEDNESDAY") {
+    document.getElementById("msgout").innerHTML = "Wndnesday's child is full pf woe";
+  } else if (selectDay === "THURSDAY") {
+    document.getElementById("msgout").innerHTML = "Thursday's child has far to go";
+  } else if (selectDay === "FRIDAY") {
+    document.getElementById("msgout").innerHTML = "Firday's child is loving and giving";
+  }
+});
 
 let resetBtn = document.getElementById("rst");
 
 resetBtn.addEventListener("click",function(){
-  
+
 })
+
