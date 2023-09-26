27c95ce094418e574d621378655766e0ce82a9e9
KyLLlHEP <kushnerartur@gmail.com>
2023-09-26 11:09:56 +0100

    feat: get element

---
+++ js/money_script.js
@@ -0,0 +1,7 @@
+let currencyElem = document.getElementById("currency");
+
+let calculateBtn = document.getElementById("currencybtn");
+
+let showResult50 = document.getElementById("output50");
+let showResult20 = document.getElementById("output20");
+let showResult10 = document.getElementById("output10");
