0eb2e185931ea2515604131b5b15f697fc25c7de
KyLLlHEP <kushnerartur@gmail.com>
2023-09-26 11:55:09 +0100

    feat: All programm done with describe

--- js/money_script.js
+++ js/money_script.js
@@ -1,3 +1,4 @@
+// Get DOM elements
 let currencyElem = document.getElementById("currency");
 
 let calculateBtn = document.getElementById("currencybtn");
@@ -5,3 +6,36 @@ let calculateBtn = document.getElementById("currencybtn");
 let showResult50 = document.getElementById("output50");
 let showResult20 = document.getElementById("output20");
 let showResult10 = document.getElementById("output10");
+
+// Function for calculating the number of coins of different denominations from a given amount in pence
+function calculateCoins(pence) {
+  let coins50 = Math.floor(pence / 50);// Number of 50p coins
+  pence %= 50;// Update the amount by reducing it by the number of 50p coins already calculated
+  
+  let coins20 = Math.floor(pence / 20);// Number of 20p coins
+  pence %= 20;// Update the amount by reducing it by the number of 20p coins already calculated
+  
+  let coins10 = Math.floor(pence / 10);// Number of 10p coins
+  pence %= 10;// Update the amount by reducing it by the number of 10p coins already calculated
+  
+  
+  return {
+      coins50: coins50,
+      coins20: coins20,
+      coins10: coins10
+  };
+}
+
+// Click event handler for the calculateBtn button
+calculateBtn.addEventListener("click", function() {
+  // Convert the value from the input field to a number and convert this amount to pence
+    let totalPounds = parseFloat(currencyElem.value);
+    let totalPence = totalPounds * 100;
+  // Call the calculateCoins function to get the number of coins
+  let results = calculateCoins(totalPence);
+  
+  // Display results on the page
+    showResult50.textContent = `There are ${results.coins50} fifty pence pieces in £${totalPounds}`;
+    showResult20.textContent = `There are ${results.coins20} twenty pence pieces in £${totalPounds}`;
+    showResult10.textContent = `There are ${results.coins10} ten pence pieces in £${totalPounds}`;
+})
