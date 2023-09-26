20154480c42347ea1cdad49bc24de17749a3cd02
KyLLlHEP <kushnerartur@gmail.com>
2023-09-26 10:19:09 +0100

    feat: add event elemnt

---
+++ js/nursery-script.js
@@ -0,0 +1,11 @@
+let menuselectElm = document.getElementById("menuselect");
+
+menuselectElm.addEventListener("change",function(){
+
+})
+
+let resetBtn = document.getElementById("rst");
+
+resetBtn.addEventListener("click",function(){
+  
+})
