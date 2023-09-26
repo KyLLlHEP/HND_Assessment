c9aee0f19f83808e5c48a48931edb16555a3193c
KyLLlHEP <kushnerartur@gmail.com>
2023-09-26 09:03:26 +0100

    feat: add subscribe code

--- assess_tasks/assess_tasks/js/subscibe_script.js
+++ assess_tasks/assess_tasks/js/subscibe_script.js
@@ -1,7 +1,14 @@
+// To make the code more readable and easy to maintain, the main logic was split into several functions.
+//   Dividing the code into small functions makes it easy to track each validation step and simplifies possible future revisions.
+//   Each function performs its own specific task, which avoids code duplication and makes it easier to understand.
+
+
+
 let emailElem = document.getElementById("email");
 let usernameElem = document.getElementById("username");
 let sub = document.getElementById("sub");
 
+//Email format validation
 function validateEmail(emailValue) {
   let emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
   if (!emailPattern.test(emailValue)) {
@@ -12,6 +19,7 @@ function validateEmail(emailValue) {
   return true;
 }
 
+// User name validation (only letters and numbers are allowed)
 function validateUsername(usernameValue) {
   let usernamePattern = /^[a-zA-Z0-9]+$/;
   if (!usernamePattern.test(usernameValue)) {
@@ -22,12 +30,16 @@ function validateUsername(usernameValue) {
   return true;
 }
 
+
+
+// Event handler of the event of pressing the "Subscribe" button
 sub.addEventListener("click", function(event) {
   event.preventDefault();
 
   let emailValue = emailElem.value;
   let usernameValue = usernameElem.value;
   
+   // If both fields pass validation, redirect the user to the subbed.html page
   if (validateEmail(emailValue) && validateUsername(usernameValue)) {
     window.location.href = "subbed.html";
   }
@@ -35,6 +47,7 @@ sub.addEventListener("click", function(event) {
 
 let btnReset = document.getElementById("btn");
 
+// Event handler of the event of pressing the "Reset" button
 btnReset.addEventListener("click", function(event) {
   event.preventDefault();
   emailElem.value = "";
